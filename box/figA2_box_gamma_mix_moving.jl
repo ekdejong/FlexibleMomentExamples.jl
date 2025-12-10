06d29d6d22c6aa75ddd2ad828392dcdfe2448888
dejong5 <dejong5@llnl.gov>
2025-12-10 14:15:47 -0800

    add moving threshold example

--- box/figA2_box_gamma_mix_moving.jl
+++ box/figA2_box_gamma_mix_moving.jl
@@ -1,9 +1,10 @@
-"Box model with two gamma modes"
+"Box model with four gamma modes, percentile thresholds, & Golovin kernel"
 
 using OrdinaryDiffEq
 
 include("./utils/box_model_helpers.jl")
 include("./utils/plotting_helpers.jl")
+include("./utils/netcdf_helpers.jl")
 
 FT = Float64
 
@@ -21,17 +22,19 @@ dist_init = (
 kernel_func = LinearKernelFunction(FT(5)) # 5 m^3/kg/s; x, y in kg
 kernel = CoalescenceTensor(kernel_func, 1, FT(1e-6))
 tspan = (FT(0), FT(120))
+
 NProgMoms = map(dist_init) do dist
     nparams(dist)
 end
 norms = (1e6, 1e-9) # 1e6/m^3; 1e-9 kg
-coal_data = CoalescenceData(kernel, NProgMoms, (FT(1e-9), FT(1e-7), FT(1e-5), Inf), norms)
-rhs = make_box_model_rhs(AnalyticalCoalStyle())
+coal_data = CoalescenceData(kernel, NProgMoms, (0.99, 0.99, 0.99, 1.0), norms, MovingThreshold())
+rhs = make_box_model_rhs(AnalyticalCoalStyle(), MovingThreshold())
 ODE_parameters = (; pdists = dist_init, coal_data = coal_data, NProgMoms = NProgMoms, norms = norms, dt = FT(1))
 prob = ODEProblem(rhs, moment_init, tspan, ODE_parameters)
 sol = solve(prob, SSPRK33(), dt = ODE_parameters.dt)
 
-plot_params!(sol, ODE_parameters; file_name = "../figures/fig3/box_gamma_mixture_4modes_params.pdf", yscale = :identity)
-plot_moments!(sol, ODE_parameters; file_name = "../figures/fig3/box_gamma_mixture_4modes_moments.pdf")
-plot_spectra!(sol, ODE_parameters; file_name = "../figures/fig3/box_gamma_mixture_4modes_spectra.pdf", logxrange = (-12, -3))
-#print_box_results!(sol, ODE_parameters)
+box_output(sol, ODE_parameters, "../results/moving_gamma_golovin.nc", FT)
+plot_params!(sol, ODE_parameters; file_name = "../figures/figA2/moving_gamma_golovin_params.pdf", yscale=:identity)
+plot_moments!(sol, ODE_parameters; file_name = "../figures/figA2/moving_gamma_golovin_moments.pdf")
+plot_spectra!(sol, ODE_parameters; file_name = "../figures/figA2/moving_gamma_golovin_spectra.pdf", logxrange = (-12, -3))
+
